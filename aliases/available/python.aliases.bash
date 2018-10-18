6ec99c6153bc631d778efafa22c804c60e0f708f
Tracy Flynn <tracysflynn@gmail.com>
2018-10-18 10:25:27 -0400

    Cleaned up duplicate code for python build aliases and plugins

--- aliases/available/python.aliases.bash
+++ aliases/available/python.aliases.bash
@@ -5,38 +5,12 @@ about-alias 'Python aliases'
 # Python
 #
 
-function pipf() {
-  if ! [ -z "$1" ]; then
-    pip list | grep -i "$1"
-  fi
-}
-
-function pipl() {
-  pip list
-}
-
-function pip3f() {
-  if ! [ -z "$1" ]; then
-    pip3 list | grep -i "$1"
-  fi
-}
-
-function pip3l() {
-  pip3 list
-}
-
 alias pcl="python_clean"
 alias pypa_build="python_clean && pip3 install ."
 alias pb="pypa_build"
 # alias pypa_build_pip3="python_clean && pip3 install -e ."
 # alias pb3="pypa_build_pip3"
 
-function pypa_build_wheel() {
-  python_clean
-  python3 setup.py sdist bdist_wheel
-  whl=$(find dist -name \*.whl)
-  pip3 install $whl
-}
 alias pbw="pypa_build_wheel"
 
 alias pyinit="touch __init__.py"
--- plugins/available/python.plugin.bash
+++ plugins/available/python.plugin.bash
@@ -1,10 +1,24 @@
 cite about-plugin
 about-plugin 'Additional python functions'
 
+function pipf() {
+  if ! [ -z "$1" ]; then
+    pip list | grep -i "$1"
+  fi
+}
+
+function pipl() {
+  pip list
+}
+
 function pip3f() {
   if ! [ -z "$1" ]; then
     pip3 list | grep -i "$1"
   fi
+}
+
+function pip3l() {
+  pip3 list
 }
 
 function pypa_build_wheel() {
@@ -12,5 +26,5 @@ function pypa_build_wheel() {
   python3 setup.py sdist bdist_wheel
   whl=$(find dist -name \*.whl)
   pip3 install $whl
+  python_clean
 }
-
